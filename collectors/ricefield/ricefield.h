4ac668d22d0d34e24a5918deb1b71101fd87e0f1
Halfdan Rump <halfdan-rump@garage.co.jp>
2015-04-10 15:02:15 +0900

    Commits changes in libs

--- collectors/ricefield/ricefield.h
+++ collectors/ricefield/ricefield.h
@@ -1,15 +1,2 @@
-void sleepMCU();
-void sleep_radio();
-void wakeup_radio();
-
-// void doStuff();
-void rtcInterrupt();
-
-void read_sensors();
-
-void read_temperature();
-void read_humidity();
-void read_sonar_distance();
-
 void init_sdcard();
 void writeData(unsigned char *buffer);
