aefb2a135f36c44d376a12605d29cd48cea1b84b
Halfdan Rump <halfdan-rump@garage.co.jp>
2015-07-24 16:00:43 +0900

    Moves sd code out of the ricefield sketch

--- collectors/ricefield/ricefield.cpp
+++ collectors/ricefield/ricefield.cpp
@@ -12,17 +12,17 @@
 
 #define FILENAME "data.txt"
 
-SdFat sd;
-SdFile myFile;
+// SdFat sd;
+// SdFile myFile;
 
-Saboten *board = new Saboten(57600);
+Saboten *board = new Saboten(1, 57600);
 
 int sdDetectPin = 19;
 int sdCsPin = 15;
 
 void setup()
 {
-  // board->register_sensor(new Paralax28015REVC_Sensor(5));  
+  board->register_sensor(new Paralax28015REVC_Sensor(1, 5));  
   // board->register_sensor(new DHT_V12_Sensor(6));  
   chibiCmdInit(57600);
   init_sdcard();
@@ -39,7 +39,7 @@ void loop()
 { 
   uint8_t tx_buf[TX_LENGTH];
   memset(tx_buf, 0, TX_LENGTH);
-  
+  // std::string buffer;
   board->read_sensors(tx_buf);
   board->read_board_diagnostics(tx_buf);
   
@@ -65,61 +65,61 @@ void loop()
 
 void init_sdcard(){
 // set up sd card detect
-  pinMode(sdDetectPin, INPUT);
-  digitalWrite(sdDetectPin, HIGH);
-  // delay(1000);
-  // check for SD and init
-  int sdDetect = digitalRead(sdDetectPin);
-  // Serial.println(sdDetect);
-  if (sdDetect == 0){
-    // init the SD card
-    if (!sd.begin(sdCsPin)){
+  // pinMode(sdDetectPin, INPUT);
+  // digitalWrite(sdDetectPin, HIGH);
+  // // delay(1000);
+  // // check for SD and init
+  // int sdDetect = digitalRead(sdDetectPin);
+  // // Serial.println(sdDetect);
+  // if (sdDetect == 0){
+  //   // init the SD card
+  //   if (!sd.begin(sdCsPin)){
       
-      Serial.println("Card failed, or not present");
-      sd.initErrorHalt();
-      return;
-    }
-    Serial.println("SD Card is initialized.\n");
-  }
-  else{
-    Serial.println("No SD card detected.\n");
-  }
+  //     Serial.println("Card failed, or not present");
+  //     sd.initErrorHalt();
+  //     return;
+  //   }
+  //   Serial.println("SD Card is initialized.\n");
+  // }
+  // else{
+  //   Serial.println("No SD card detected.\n");
+  // }
 
 }
 
 
 void writeData(unsigned char *buffer){    
-    int open_success = myFile.open(FILENAME, O_RDWR | O_CREAT | O_AT_END);
-    // Serial.print("Open file: ");
-    // Serial.println(open_success);
-    if (open_success){
-      // std::string str(buffer);
+    // int open_success = myFile.open(FILENAME, O_RDWR | O_CREAT | O_AT_END);
+    // // Serial.print("Open file: ");
+    // // Serial.println(open_success);
+    // if (open_success){
+    //   // std::string str(buffer);
       
-      char data[100];
-      strcpy(data, (char*) buffer);
-      Serial.println("Writing data to SD card ");
-      // Serial.println(data); 
-      // int n = sizeof(buffer);
-      // Serial.println(n);
+    //   char data[100];
+    //   strcpy(data, (char*) buffer);
+    //   Serial.println("Writing data to SD card ");
+    //   // Serial.println(data); 
+    //   // int n = sizeof(buffer);
+    //   // Serial.println(n);
 
       
-      myFile.println(data);
+    //   myFile.println(data);
 
-      // char * ptr;
-      // char array[10];
-      // // Populate ptr
-      // ptr = "10234";
-      // // Copy from ptr to array.
-      // strcpy(array, ptr);      
-      // Serial.println("Wrote data");
+    //   // char * ptr;
+    //   // char array[10];
+    //   // // Populate ptr
+    //   // ptr = "10234";
+    //   // // Copy from ptr to array.
+    //   // strcpy(array, ptr);      
+    //   // Serial.println("Wrote data");
 
-      myFile.close();
-      Serial.println("Closed file");
+    //   myFile.close();
+    //   Serial.println("Closed file");
      
-    }
-    else{
-      Serial.println("Error opening dataFile");
-    }
+    // }
+    // else{
+    //   Serial.println("Error opening dataFile");
+    // }
 }
 
 
