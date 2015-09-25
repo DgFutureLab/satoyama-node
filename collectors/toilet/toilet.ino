de7faa22141fa4dc2568e591cd9e2479346adfc4
Halfdan Rump <halfdan-rump@garage.co.jp>
2015-09-25 17:21:46 +0900

    Toilets sketch now posts occupied/unoccupied to the API

--- collectors/toilet/toilet.ino
+++ collectors/toilet/toilet.ino
@@ -1,6 +1,12 @@
+#define NODE_ID 4
+#define SONAR_SENSOR_ID 10
+
+/*
+ Constants and variables for sonar stuff
+*/
+
 #define trigPin 13
 #define echoPin 12
-
 int old_distance;
 int new_distance;
 int change;
@@ -9,11 +15,29 @@ const int ping_delay_msec = 2000;
 const int dist_n_avr = 10;
 boolean occupied;
 
+
+
+/*
+ Constants and variables for 3G stuff
+*/
+
+#include <SoftwareSerial.h>
+#include "a3gim.h"
+
+const char *server = "satoyamacloud.com";
+int port = 80;
+char *path;
+char res[a3gsMAX_RESULT_LENGTH+1];
+int len;
+
+
+
 void setup() {
   Serial.begin (9600);
   Serial.println("BEGIN");
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  init_3G_connection();
 }
 
 
@@ -23,6 +47,7 @@ void loop() {
   old_distance = new_distance;
   new_distance = measure_distance();
   change = new_distance - old_distance;
+
   if (abs(change) > tolerance){
     if(change > 0){
       occupied = 0;
@@ -35,9 +60,40 @@ void loop() {
   }
 }
 
-void send_toilet_status(boolean occupied){
 
+
+void send_toilet_status(boolean occupied){
+//  char path[100];
+  if(occupied){
+    path = "/reading/1?sensor_id=10&value=1";
+//    printf("/reading/%u?sensor_id=4&value=1", NODE_ID);
+  } else{
+    path = "/reading/1?sensor_id=10&value=0";
+  }
+ 
+ if (a3gs.httpGET(server, port, path, res, len) == 0) {
+      Serial.println("OK!");
+      Serial.print("[");
+      Serial.print(res);
+      Serial.println("]");
+    }
+    else {
+      Serial.print("Can't get HTTP response from ");
+      Serial.println(server);
+    } 
+  
 }
+
+void init_3G_connection(){
+  Serial.println("Starting 3G shield...");
+  if (a3gs.start() == 0 && a3gs.begin() == 0) {
+  Serial.println("Succeeded.");
+  } else{
+    Serial.println("Failed.");
+  }
+}
+
+
 
 int measure_distance(){
   long duration, distance;
